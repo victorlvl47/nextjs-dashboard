286346b123e594d08eb7b08b77fbbd824f2da6cd
victorlvl47 <victor.lvl47@gmail.com>
2025-09-07 21:20:36 -0600

    completed chapter 7

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -19,7 +19,27 @@ export async function fetchRevenue() {
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    const data = await sql<Revenue[]>`SELECT * FROM revenue`;
+    // const data = await sql<Revenue[]>`SELECT * FROM revenue`;
+
+    const data = await sql<Revenue[]>`
+      SELECT *
+      FROM revenue
+      ORDER BY 
+        CASE month
+          WHEN 'Jan' THEN 1
+          WHEN 'Feb' THEN 2
+          WHEN 'Mar' THEN 3
+          WHEN 'Apr' THEN 4
+          WHEN 'May' THEN 5
+          WHEN 'Jun' THEN 6
+          WHEN 'Jul' THEN 7
+          WHEN 'Aug' THEN 8
+          WHEN 'Sep' THEN 9
+          WHEN 'Oct' THEN 10
+          WHEN 'Nov' THEN 11
+          WHEN 'Dec' THEN 12
+        END;
+    `;
 
     // console.log('Data fetch completed after 3 seconds.');
 
